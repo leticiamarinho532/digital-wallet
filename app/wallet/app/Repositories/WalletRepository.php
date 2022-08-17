5ebf35d8adb64b451de6671c4fde5fd83b74e450
Leticia <leticia.marinho532@gmail.com>
2022-08-17 03:13:02 -0300

    Add new transaction methods to wallet repository

--- app/wallet/app/Repositories/WalletRepository.php
+++ app/wallet/app/Repositories/WalletRepository.php
@@ -12,4 +12,25 @@ class WalletRepository implements WalletRepositoryInterface
     {
         return Wallet::where('user_id', $userId);
     }
+
+    public function startTransactionWatch()
+    {
+        return \DB::beginTransaction();
+    }
+
+    public function updateBalance($userId, $value)
+    {
+        return Wallet::where('user_id', $userId)
+            ->update(['balance' => $value]);
+    }
+
+    public function commitTransactionWatch()
+    {
+        return \DB::commit();
+    }
+
+    public function rollBackTransactionWatch()
+    {
+        return \DB::rollBack();
+    }
 }
